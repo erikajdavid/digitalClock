4b27b327ed9aa0bfede972f1b65d7289ddbe36bf
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-05-18 14:51:03 -0400

    pseudocdode added

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+//base
+//00:00:00AM/PM
+//hr/min/sec
+
+//function to set up seconds
+//seconds
+    //00 -> 59
+        //after 59, go back to 00, but add increment to min
+
+//function to set up minutes
+//minutes
+    //00 -> 59
+        //after 59, go back to 00, but add increase to hour
+
+//function to set up hours
+//hours
+    //00 -> 23
+        //after 23:59:59, go back to 00:00:00
+
+//AM 00:00:00 to 11:59:59
+//PM 12:00:00 to 23:59:59 
+
+//function to restart/reset
+
+
+//questions?
+
+    
+
+

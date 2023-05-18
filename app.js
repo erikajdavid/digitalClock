e193ede24b96e63253f64ed40ae01e9ba11e3e7d
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-05-18 16:43:24 -0400

    seconds function

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 
 
 //base
-//00:00:00AM/PM
+//00:00:00
 //hr/min/sec
 
 //function to set up seconds
 //seconds
     //00 -> 59
         //after 59, go back to 00, but add increment to min
+
+const secondsEl = document.querySelector('.seconds');
+
+let secondsGoingBy = 00;
+
+function startSeconds() {
+    secondsGoingBy++;
+    secondsEl.textContent = secondsGoingBy;
+    if (secondsGoingBy === 59) {
+        secondsGoingBy === 00;
+    }
+}
+
+let secondsInterval = null;
+function secondsSpeed() {
+    secondsInterval = setInterval(startSeconds, 1000);
+}
+secondsSpeed();
 
 //function to set up minutes
 //minutes
